8c97c40950c88dbcee4083e6674a844295ec3c48
Gabe Godoi <gabegodoi@gmail.com>
2024-10-29 17:47:41 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CharacterList from './components/CharacterList.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Marvel Characters</h1>
+      <CharacterList />
+    </div>
+  );
+}
+
+export default App;
